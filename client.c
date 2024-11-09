15ef8f93b74382bb034b16302ce403446320e8aa
Muhammad Naeemuddin <muhammedalam150@gmail.com>
2024-11-09 15:27:57 +0500

    Update client.c

--- client.c
+++ client.c
@@ -9,19 +9,33 @@ int main() {
     struct sockaddr_in server_addr;
     char buffer[1024];
     
+    // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    // Set server address information
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
-    inet_pton(AF_INET, "192.168.56.101", &server_addr.sin_addr); // Server IP
+    inet_pton(AF_INET, "10.0.2.6", &server_addr.sin_addr);  // Use the server's IP address here
 
-    connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection to server failed");
+        return 1;
+    }
 
+    // Send a "ping" message to the server
     strcpy(buffer, "ping");
     send(sockfd, buffer, strlen(buffer), 0);
     
+    // Receive the timestamp from the server
     recv(sockfd, buffer, sizeof(buffer), 0);
     printf("Server time: %s\n", buffer);
 
+    // Close the connection
     close(sockfd);
     return 0;
 }
